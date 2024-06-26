a5aad22984e27091414e6421cf8a75169d148e31
mdiannibelli <mdiannibelli@gmail.com>
2024-06-26 19:07:43 -0300

    responsive & gallery

--- src/components/UI/Mockup.tsx
+++ src/components/UI/Mockup.tsx
@@ -24,30 +24,54 @@ const Mockup = () => {
         },
     ]
     return (
-        <section className="grid grid-cols-3 w-full h-full my-8 bg-black">
-            {mockups.map((mockup, index) => (
-                <div
-                    key={index}
-                    className="relative"
-                    onMouseEnter={() => setHoveredIndex(index)}
-                    onMouseLeave={() => setHoveredIndex(null)}
-                >
-                    {
-                        hoveredIndex === index && (
-                            <div>
-                                <div className="absolute top-[14rem] z-10 w-full animate-fade-in-right">
-                                    <h1 className="px-2 text-4xl 2xl:text-6xl uppercase text-primary text-center font-semibold ">{mockup.title}</h1>
-                                    <span className="text-xs lg:text-md 2xl:text-2xl tracking-[0.5rem] 2xl:tracking-[1.2rem] font-light flex justify-center text-white uppercase">{mockup.subtitle}</span>
+        <section className="mt-8">
+            <div className="hidden md:grid md:grid-cols-3 w-full h-full my-8 bg-black">
+                {mockups.map((mockup, index) => (
+                    <div
+                        key={index}
+                        className="relative"
+                        onMouseEnter={() => setHoveredIndex(index)}
+                        onMouseLeave={() => setHoveredIndex(null)}
+                    >
+                        {
+                            hoveredIndex === index && (
+                                <div>
+                                    <div className="absolute top-[14rem] z-10 w-full animate-fade-in-right">
+                                        <h1 className="px-2 text-4xl 2xl:text-6xl uppercase text-primary text-center font-semibold ">{mockup.title}</h1>
+                                        <span className="text-xs lg:text-md 2xl:text-2xl tracking-[0.5rem] 2xl:tracking-[1.2rem] font-light flex justify-center text-white uppercase">{mockup.subtitle}</span>
+                                    </div>
+                                    <div className="absolute top-[24rem] left-4 lg:left-6 2xl:left-12 z-10 animate-fade-in-left">
+                                        <p className="text-xs 2xl:text-md text-white font-light text-center md:max-w-[220px] lg:max-w-[300px] 2xl:max-w-[520px]">{mockup.description}</p>
+                                    </div>
                                 </div>
-                                <div className="absolute top-[24rem] left-4 lg:left-6 2xl:left-12 z-10 animate-fade-in-left">
-                                    <p className="text-xs 2xl:text-md text-white font-light text-center md:max-w-[220px] lg:max-w-[300px] 2xl:max-w-[520px]">{mockup.description}</p>
-                                </div>
+                            )
+                        }
+                        <div className={`${mockup.bg} ${hoveredIndex === index && 'saturate-120 sepia-[0] opacity-30'} sepia-[.95] h-[720px]  w-full opacity-[0.5] bg-center object-cover bg-no-repeat duration-700 transition-all`} />
+                    </div>
+                ))}
+            </div>
+
+            <div
+                className="relative flex w-full md:hidden"
+                onMouseEnter={() => setHoveredIndex(0)}
+                onMouseLeave={() => setHoveredIndex(null)}
+            >
+                {
+                    hoveredIndex === 0 && (
+                        <div>
+                            <div className="absolute top-[14rem] z-10 w-full animate-fade-in-right">
+                                <h1 className="px-2 text-5xl uppercase text-primary text-center font-semibold ">Hard Rock</h1>
+                                <span className="text-lg tracking-[0.5rem] font-light flex justify-center text-white uppercase">Cheese burguer</span>
                             </div>
-                        )
-                    }
-                    <div className={`${mockup.bg} ${hoveredIndex === index && 'saturate-120 sepia-[0] opacity-30'} sepia-[.95] h-[720px]  w-full opacity-[0.5] bg-center object-cover bg-no-repeat duration-700 transition-all`} />
-                </div>
-            ))}
+                            <div className="absolute top-[24rem] flex justify-center items-center w-full z-10 animate-fade-in-right">
+                                <p className="text-xs text-white font-light text-center max-w-xs">Queso provoleta con chimichurri, morcilla, morrón a la plancha, cebolla roja y ketchup ahumado</p>
+                            </div>
+                        </div>
+                    )
+                }
+                <div className={`bg-mockup ${hoveredIndex === 0 && 'saturate-120 sepia-[0] opacity-30'} sepia-[.95] h-[720px]  w-full opacity-[0.5] bg-center object-cover bg-no-repeat duration-700 transition-all`} />
+            </div>
+
         </section>
     )
 }
