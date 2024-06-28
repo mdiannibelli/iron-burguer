1983cab4e58566ac0299e4531bd35a1b7d1469dc
mdiannibelli <mdiannibelli@gmail.com>
2024-06-28 17:09:24 -0300

    fix public imgs routes

--- src/components/UI/SliderMenus.tsx
+++ src/components/UI/SliderMenus.tsx
@@ -13,9 +13,9 @@ const SliderMenus = () => {
     return (
         <div>
             <Swiper navigation={true} modules={[Navigation]} className="mySwiper flex justify-center items-center max-w-[500px] my-8">
-                <SwiperSlide className='flex justify-center items-center'><img src={'/public/imgs/menu1.jpg'} alt='Menu 1' className='mx-auto w-[350px] h-[600px] object-cover' /></SwiperSlide>
-                <SwiperSlide className='flex justify-center items-center'><img src={'/public/imgs/menu2.jpg'} alt='Menu 2' className='mx-auto w-[350px] h-[600px] object-cover' /></SwiperSlide>
-                <SwiperSlide className='flex justify-center items-center'><img src={'/public/imgs/menu3.jpg'} alt='Menu 3' className='mx-auto w-[350px] h-[600px] object-cover' /></SwiperSlide>
+                <SwiperSlide className='flex justify-center items-center'><img src={'./imgs/menu1.jpg'} alt='Menu 1' className='mx-auto w-[350px] h-[600px] object-cover' /></SwiperSlide>
+                <SwiperSlide className='flex justify-center items-center'><img src={'./imgs/menu2.jpg'} alt='Menu 2' className='mx-auto w-[350px] h-[600px] object-cover' /></SwiperSlide>
+                <SwiperSlide className='flex justify-center items-center'><img src={'./imgs/menu3.jpg'} alt='Menu 3' className='mx-auto w-[350px] h-[600px] object-cover' /></SwiperSlide>
             </Swiper>
         </div>
     )
